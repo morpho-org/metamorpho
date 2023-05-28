0215d3fe20f5f3dfeef40ced5a759e48cf5943fc
Rubilmax <rmilon@gmail.com>
2023-05-28 10:11:01 +0200

    feat(gradient): calculate final allocation

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import { maximize } from "./gradient-descent";
 
-const L = 10;
+const L = 900;
 const dt = 1; // 1 year
 
 const pools = [
@@ -12,20 +12,27 @@ const pools = [
 
 const dim = pools.length;
 
-const interests = (x: number, a: number, b: number) => (a / (b + x)) * x * dt;
+const interests = (x: number, a: number, b: number) => {
+  if (Math.abs(x) >= Infinity) return a * dt - poolGasCost * gasPrice; // first-order approximation
+
+  return (a / (b + x)) * x * dt;
+};
 const dInterests = (x: number, a: number, b: number) => (a * b * dt) / (b + x) ** 2;
 
-const nu = (x: number, a: number, b: number) => {
-  if (Math.abs(x) >= Infinity) return 1; // first-order approximation
+const gasPenaltySlope = 1e3;
+const depositThreshold = L / 200;
 
-  return interests(x, a, b);
-};
+const poolGasCost = 80_000; // arbitrary value
+const gasPrice = 30e-9; // let's assume asset is ETH and gas price is 30 gwei
 
-const dNu = (x: number, a: number, b: number) => {
-  if (Math.abs(x) >= Infinity) return 0; // first-order approximation
+const gasPenalty = (x: number) =>
+  (poolGasCost * gasPrice) / (1 + Math.exp(-gasPenaltySlope * (x - depositThreshold)));
+const dGasPenalty = (x: number) =>
+  (gasPenaltySlope * poolGasCost * gasPrice * Math.exp(-gasPenaltySlope * (x - depositThreshold))) /
+  (1 + Math.exp(-gasPenaltySlope * (x - depositThreshold))) ** 2;
 
-  return dInterests(x, a, b);
-};
+const nu = (x: number, a: number, b: number) => interests(x, a, b) - gasPenalty(x);
+const dNu = (x: number, a: number, b: number) => dInterests(x, a, b) - dGasPenalty(x);
 
 const { x, i } = maximize(
   (x) =>
@@ -43,11 +50,11 @@ const { x, i } = maximize(
       return dNu(xi, a, b) * L;
     });
 
-    let nH2 = dim; // norm 2 of liquidity hyperplane's normal vector
-    let dotH = grad.reduce((g, tot) => g + tot); // dot product of gradient and liquidity hyperplane's normal vector
-    let gradH = grad.map((g) => g - dotH / nH2); // gradient projected on liquidity hyperplane
+    let nH2 = dim; // norm 2 of constant sum hyperplane's normal vector
+    let dotH = grad.reduce((g, tot) => g + tot); // dot product of gradient and constant sum hyperplane's normal vector
+    let gradH = grad.map((g) => g - dotH / nH2); // gradient projected on constant sum hyperplane
 
-    let vH = new Array(dim).fill(1); // constraint sub vector space's definition vector
+    let vH = new Array(dim).fill(1); // constant sum sub vector space's definition vector
 
     let underflow = false;
     do {
@@ -75,11 +82,32 @@ const { x, i } = maximize(
 
 console.log(x, i);
 
-const totalInterests = x.map((xi, i) => {
+let nH2 = dim; // norm 2 of liquidity hyperplane's normal vector
+let xH = x.slice();
+let vH = new Array(dim).fill(1); // constant sum sub vector space's definition vector
+
+for (let i = 0; i < dim; ++i) {
+  if (xH[i] < depositThreshold) {
+    xH[i] = 0;
+    vH[i] = 0;
+    nH2 -= 1;
+  }
+}
+
+if (nH2 <= 0) xH.fill(0);
+else {
+  const dotH = xH.reduce((xi, tot) => xi + tot) - L; // dot product of x and liquidity sub vector space's definition vector
+  xH = xH.map((xi, i) => xi - (dotH * vH[i]) / nH2); // x projected on sub vector space
+}
+
+console.log(xH);
+
+const totalInterests = xH.map((xi, i) => {
   const [a, b] = pools[i];
 
   return interests(xi, a, b);
 });
 const totalAccrued = totalInterests.reduce((a, tot) => a + tot);
+const cost = nH2 * poolGasCost * gasPrice;
 
-console.log(totalInterests, totalAccrued, (totalAccrued * 100) / (L * dt));
+console.log(totalInterests, totalAccrued - cost, ((totalAccrued - cost) * 100) / (L * dt));
