60c70fd8bd7f074fc9ca3cbcdd6dc774ec96cab1
Rubilmax <rmilon@gmail.com>
2023-08-02 15:16:15 +0200

    feat(bulker): added optimism

---
+++ contracts/bulker/optimism-mainnet/OptimismBulker.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {AaveV3Bulker} from "../AaveV3Bulker.sol";
+import {BalancerBulker} from "../BalancerBulker.sol";
+
+import {BlueBulker} from "../BlueBulker.sol";
+import {ERC20Bulker} from "../ERC20Bulker.sol";
+import {WNativeBulker} from "../WNativeBulker.sol";
+
+contract OptimismBulker is ERC20Bulker, WNativeBulker, BlueBulker, AaveV3Bulker, BalancerBulker {
+    constructor(address blue)
+        WNativeBulker(0x4200000000000000000000000000000000000006)
+        BlueBulker(blue)
+        AaveV3Bulker(0x794a61358D6845594F94dc1DB02A252b5b4814aD)
+        BalancerBulker(0xBA12222222228d8Ba445958a75a0704d566BF2C8)
+    {}
+
+    function _dispatch(Action memory action)
+        internal
+        override(ERC20Bulker, WNativeBulker, BlueBulker, AaveV3Bulker, BalancerBulker)
+        returns (bool)
+    {
+        return super._dispatch(action);
+    }
+}
