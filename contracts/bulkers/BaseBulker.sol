ddcbc5540a5e11fd799326d01bcea694f69eff43
Rubilmax <rmilon@gmail.com>
2023-08-11 09:08:28 +0200

    refactor(bulker): fix errors import

--- contracts/bulkers/BaseBulker.sol
+++ contracts/bulkers/BaseBulker.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.21;
 
 import {IMulticall} from "./interfaces/IMulticall.sol";
 
+import {Errors} from "./libraries/Errors.sol";
+
 import {SelfMulticall} from "./SelfMulticall.sol";
 
 /// @title BaseBulker.
