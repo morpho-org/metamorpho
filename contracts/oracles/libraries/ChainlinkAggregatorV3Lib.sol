f04e2b07f327dde5a118c4e2dad48aaf8571127d
MerlinEgalite <egalite.merlin@gmail.com>
2023-08-20 12:02:22 +0200

    feat: overload price function in adapter

--- contracts/oracles/libraries/ChainlinkAggregatorV3Lib.sol
+++ contracts/oracles/libraries/ChainlinkAggregatorV3Lib.sol
@@ -4,9 +4,28 @@ pragma solidity ^0.8.0;
 import {IChainlinkAggregatorV3} from "../adapters/interfaces/IChainlinkAggregatorV3.sol";
 
 library ChainlinkAggregatorV3Lib {
-    function price(IChainlinkAggregatorV3 aggregator) internal view returns (uint256) {
-        (, int256 answer,,,) = aggregator.latestRoundData();
-        require(answer > 0, "ChainlinkAggregatorV3Lib: price is negative");
-        return uint256(answer);
+    function price(IChainlinkAggregatorV3 dataFeed) internal view returns (uint256) {
+        (, int256 data,,,) = dataFeed.latestRoundData();
+        require(data > 0, "ChainlinkAggregatorV3Lib: price is negative");
+        return uint256(data);
+    }
+
+    function price(IChainlinkAggregatorV3 dataFeed, IChainlinkAggregatorV3 sequencerUptimeFeed, uint256 gracePeriod)
+        internal
+        view
+        returns (uint256)
+    {
+        (, int256 answer, uint256 startedAt,,) = sequencerUptimeFeed.latestRoundData();
+
+        // answer == 0: Sequencer is up.
+        // answer == 1: Sequencer is down.
+        require(answer == 0, "ChainlinkAggregatorV3Lib: sequencer is down");
+
+        // Make sure the grace period has passed after the sequencer is back up.
+        require(block.timestamp - startedAt > gracePeriod, "ChainlinkAggregatorV3Lib: grace period not over");
+
+        (, int256 data,,,) = dataFeed.latestRoundData();
+        require(data > 0, "ChainlinkAggregatorV3Lib: price is negative");
+        return uint256(data);
     }
 }
