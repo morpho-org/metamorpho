48e3be6b7d36114cf6e9f1740d5979c13a1e1a25
MerlinEgalite <egalite.merlin@gmail.com>
2023-08-23 14:22:07 +0200

    feat: correct error messages

--- contracts/oracles/libraries/ChainlinkAggregatorV3Lib.sol
+++ contracts/oracles/libraries/ChainlinkAggregatorV3Lib.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {IChainlinkAggregatorV3} from "../adapters/interfaces/IChainlinkAggregatorV3.sol";
 import {IChainlinkOffchainAggregator} from "../adapters/interfaces/IChainlinkOffchainAggregator.sol";
 
+import {ErrorsLib} from "./ErrorsLib.sol";
+
 library ChainlinkAggregatorV3Lib {
     function price(IChainlinkAggregatorV3 priceFeed) internal view returns (uint256) {
         (, int256 answer,,,) = priceFeed.latestRoundData();
@@ -12,7 +14,7 @@ library ChainlinkAggregatorV3Lib {
         int192 minAnswer = IChainlinkOffchainAggregator(offchainFeed).minAnswer();
         int192 maxAnswer = IChainlinkOffchainAggregator(offchainFeed).maxAnswer();
 
-        require(answer >= minAnswer && answer <= maxAnswer, "ChainlinkAggregatorV3Lib: invalid answer");
+        require(answer >= minAnswer && answer <= maxAnswer, ErrorsLib.INVALID_ANSWER);
 
         return uint256(answer);
     }
@@ -26,10 +28,10 @@ library ChainlinkAggregatorV3Lib {
 
         // answer == 0: Sequencer is up.
         // answer == 1: Sequencer is down.
-        require(answer == 0, "ChainlinkAggregatorV3Lib: sequencer is down");
+        require(answer == 0, ErrorsLib.SEQUENCER_DOWN);
 
         // Make sure the grace period has passed after the sequencer is back up.
-        require(block.timestamp - startedAt > gracePeriod, "ChainlinkAggregatorV3Lib: grace period not over");
+        require(block.timestamp - startedAt > gracePeriod, ErrorsLib.GRACE_PERIOD_NOT_OVER);
 
         return price(priceFeed);
     }
--- contracts/oracles/libraries/ErrorsLib.sol
+++ contracts/oracles/libraries/ErrorsLib.sol
@@ -7,4 +7,10 @@ library ErrorsLib {
     string internal constant ZERO_INPUT = "zero input";
 
     string internal constant INVALID_QUOTE_TOKEN = "invalid quote token";
+
+    string internal constant INVALID_ANSWER = "invalid answer";
+
+    string internal constant SEQUENCER_DOWN = "sequencer down";
+
+    string internal constant GRACE_PERIOD_NOT_OVER = "grace period not over";
 }
