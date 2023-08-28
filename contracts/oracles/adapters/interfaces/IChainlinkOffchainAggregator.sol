2edd9beaf71fb61dd2f89de1e12f579f13d93567
MerlinEgalite <egalite.merlin@gmail.com>
2023-08-28 12:27:06 +0200

    chore: remove oracles

--- contracts/oracles/adapters/interfaces/IChainlinkOffchainAggregator.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0;
-
-interface IChainlinkOffchainAggregator {
-    function minAnswer() external view returns (int192);
-    function maxAnswer() external view returns (int192);
-}
