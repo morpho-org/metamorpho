a53f3eeac687797355d635646d740abd5b1d4f36
Rubilmax <rmilon@gmail.com>
2023-08-29 16:44:24 +0200

    refactor(bundler): use evm bundler instead of composition

--- contracts/bundlers/EVMBundler.sol
+++ contracts/bundlers/EVMBundler.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.21;
 
-import {MorphoBundler} from "./MorphoBundler.sol";
 import {ERC20Bundler} from "./ERC20Bundler.sol";
+import {ERC4626Bundler} from "./ERC4626Bundler.sol";
+import {MorphoBundler} from "./MorphoBundler.sol";
 
 /// @title EVMBundler
 /// @author Morpho Labs
 /// @custom:contact security@morpho.xyz
 /// @notice Common bundler layer guaranteeing it can be deployed to the same address on all EVM-compatible chains.
-contract EVMBundler is ERC20Bundler, MorphoBundler {
+contract EVMBundler is ERC20Bundler, ERC4626Bundler, MorphoBundler {
     constructor(address morpho) MorphoBundler(morpho) {}
 }
