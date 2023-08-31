ebf0cd99087051144488a751a23e0f10325f2fda
makcandrov <makcandrov@proton.me>
2023-08-31 01:45:12 -0700

    Apply suggestions
    Co-authored-by: Romain Milon <rmilon@gmail.com>
    Signed-off-by: makcandrov <makcandrov@proton.me>

--- contracts/bundlers/migration/AaveV2MigrationBundler.sol
+++ contracts/bundlers/migration/AaveV2MigrationBundler.sol
@@ -9,7 +9,7 @@ import {ILendingPool} from "@morpho-v1/aave-v2/interfaces/aave/ILendingPool.sol"
 
 import {SafeTransferLib, ERC20} from "@solmate/utils/SafeTransferLib.sol";
 
-contract AaveV2MigrationBundler is MorphoBundler, ERC4626Bundler, ERC20Bundler {
+contract AaveV2MigrationBundler is EVMBundler {
     using SafeTransferLib for ERC20;
 
     ILendingPool immutable AAVE_V2_POOl;
