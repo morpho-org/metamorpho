2b0f444301a58b297734347d8a5cf1ba94878b2d
makcandrov <makcandrov@proton.me>
2023-09-01 03:51:33 -0700

    fix: license in test files

--- test/forge/ethereum-mainnet/migration/CompoundV3MigrationBundlerTest.sol
+++ test/forge/ethereum-mainnet/migration/CompoundV3MigrationBundlerTest.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
 import {ICompoundV3} from "contracts/bundlers/migration/interfaces/ICompoundV3.sol";
