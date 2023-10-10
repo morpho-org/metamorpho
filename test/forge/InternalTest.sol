35be4758ae95f9eda5d1cd9b9498dd80e21c5176
Jean-Grimal <jeanbgrimal@gmail.com>
2023-10-10 17:40:58 +0200

    fix: apply suggestions

--- test/forge/InternalTest.sol
+++ test/forge/InternalTest.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@morpho-blue/interfaces/IMorpho.sol";
 
+import {MathLib} from "@morpho-blue/libraries/MathLib.sol";
 import {MarketParamsLib} from "@morpho-blue/libraries/MarketParamsLib.sol";
 import {MorphoLib} from "@morpho-blue/libraries/periphery/MorphoLib.sol";
 import {MorphoBalancesLib} from "@morpho-blue/libraries/periphery/MorphoBalancesLib.sol";
+import {UtilsLib} from "@morpho-blue/libraries/UtilsLib.sol";
 
 import "src/libraries/ConstantsLib.sol";
 import {ORACLE_PRICE_SCALE} from "@morpho-blue/libraries/ConstantsLib.sol";
@@ -14,7 +16,7 @@ import {IrmMock} from "src/mocks/IrmMock.sol";
 import {ERC20Mock} from "src/mocks/ERC20Mock.sol";
 import {OracleMock} from "src/mocks/OracleMock.sol";
 
-import {MetaMorpho, IMetaMorpho, ERC20, IERC20, ErrorsLib, MarketAllocation, SharesMathLib} from "src/MetaMorpho.sol";
+import {MetaMorpho, ERC20, IERC20, ErrorsLib, MarketAllocation, SharesMathLib} from "src/MetaMorpho.sol";
 
 import "@forge-std/Test.sol";
 import "@forge-std/console2.sol";
@@ -25,14 +27,17 @@ uint256 constant NB_MARKETS = MAX_QUEUE_SIZE + 1;
 uint192 constant CAP = type(uint192).max;
 
 contract InternalTest is Test, MetaMorpho {
+    using MathLib for uint256;
     using MorphoLib for IMorpho;
     using MorphoBalancesLib for IMorpho;
     using MarketParamsLib for MarketParams;
     using SharesMathLib for uint256;
+    using UtilsLib for uint256;
     using stdJson for string;
 
     address internal OWNER = makeAddr("Owner");
     address internal SUPPLIER = makeAddr("Supplier");
+    address internal BORROWER = makeAddr("Borrower");
     address internal ALLOCATOR = makeAddr("Allocator");
     address internal RISK_MANAGER = makeAddr("RiskManager");
     address internal MORPHO_OWNER = makeAddr("MorphoOwner");
@@ -69,8 +74,8 @@ contract InternalTest is Test, MetaMorpho {
         morpho.setFeeRecipient(MORPHO_FEE_RECIPIENT);
 
         changePrank(OWNER);
-        IMetaMorpho(address(this)).setRiskManager(RISK_MANAGER);
-        IMetaMorpho(address(this)).setIsAllocator(ALLOCATOR, true);
+        this.setRiskManager(RISK_MANAGER);
+        this.setIsAllocator(ALLOCATOR, true);
         vm.stopPrank();
 
         for (uint256 i; i < NB_MARKETS; ++i) {
@@ -98,6 +103,9 @@ contract InternalTest is Test, MetaMorpho {
         loanToken.approve(address(morpho), type(uint256).max);
         collateralToken.approve(address(morpho), type(uint256).max);
         vm.stopPrank();
+
+        vm.prank(BORROWER);
+        collateralToken.approve(address(morpho), type(uint256).max);
     }
 
     function _deploy(string memory artifactPath, bytes memory constructorArgs) internal returns (address deployed) {
@@ -117,9 +125,9 @@ contract InternalTest is Test, MetaMorpho {
             _setCap(id, CAP);
         }
 
-        Id lsastId = allMarkets[NB_MARKETS - 1].id();
+        Id lastId = allMarkets[NB_MARKETS - 1].id();
         vm.expectRevert(ErrorsLib.MaxQueueSizeExceeded.selector);
-        _setCap(lsastId, CAP);
+        _setCap(lastId, CAP);
     }
 
     function testSuppliableNotEnabledMarket() public {
@@ -137,28 +145,38 @@ contract InternalTest is Test, MetaMorpho {
 
         loanToken.setBalance(SUPPLIER, suppliedAmount);
         vm.prank(SUPPLIER);
-        IMetaMorpho(address(this)).deposit(suppliedAmount, SUPPLIER);
+        this.deposit(suppliedAmount, SUPPLIER);
 
         uint256 suppliable = _suppliable(allMarkets[0], id);
 
         assertEq(suppliable, CAP - suppliedAmount, "suppliable");
     }
 
-    function testWithdrawable(uint256 suppliedAmount) public {
+    function testWithdrawable(uint256 suppliedAmount, uint256 amountBorrowed) public {
         suppliedAmount = bound(suppliedAmount, MIN_TEST_ASSETS, MAX_TEST_ASSETS);
+        amountBorrowed = bound(amountBorrowed, MIN_TEST_ASSETS, suppliedAmount);
 
         Id id = allMarkets[0].id();
         _setCap(id, CAP);
 
         loanToken.setBalance(SUPPLIER, suppliedAmount);
         vm.prank(SUPPLIER);
-        IMetaMorpho(address(this)).deposit(suppliedAmount, SUPPLIER);
+        this.deposit(suppliedAmount, SUPPLIER);
+
+        uint256 collateral = suppliedAmount.wDivUp(allMarkets[0].lltv);
+        collateralToken.setBalance(BORROWER, collateral);
+
+        vm.startPrank(BORROWER);
+        morpho.supplyCollateral(allMarkets[0], collateral, BORROWER, hex"");
+        morpho.borrow(allMarkets[0], amountBorrowed, 0, BORROWER, BORROWER);
+        vm.stopPrank();
 
         uint256 withdrawable = _withdrawable(allMarkets[0], id);
 
         uint256 supplyShares = MORPHO.supplyShares(id, address(this));
         (uint256 totalSupplyAssets, uint256 totalSupplyShares,,) = MORPHO.expectedMarketBalances(allMarkets[0]);
-        uint256 expectedWithdrawable = supplyShares.toAssetsDown(totalSupplyAssets, totalSupplyShares);
+
+        uint256 expectedWithdrawable = supplyShares.toAssetsDown(totalSupplyAssets, totalSupplyShares) - amountBorrowed;
 
         assertEq(withdrawable, expectedWithdrawable, "withdrawable");
     }
