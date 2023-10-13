47717ffde51160c6f27d713263a5eb72a4ea9a6f
MerlinEgalite <egalite.merlin@gmail.com>
2023-10-13 16:28:08 +0200

    docs: constructs

--- src/MetaMorpho.sol
+++ src/MetaMorpho.sol
@@ -98,7 +98,7 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
 
     /* CONSTRUCTOR */
 
-    /// @dev Initializes the contract.
+    /// @dev Constructs the contract.
     /// @param owner The owner of the contract.
     /// @param morpho The address of the Morpho contract.
     /// @param initialTimelock The initial timelock.
