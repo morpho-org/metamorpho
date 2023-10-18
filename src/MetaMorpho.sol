ae3e2b18ae79a877ccd08596776dc750a1fc25e9
Quentin Garchery <garchery.quentin@gmail.com>
2023-10-18 12:53:46 +0200

    refactor: remove unnecessary check in _accrueFee

--- src/MetaMorpho.sol
+++ src/MetaMorpho.sol
@@ -801,7 +801,7 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
         uint256 feeShares;
         (feeShares, newTotalAssets) = _accruedFeeShares();
 
-        if (feeShares != 0 && feeRecipient != address(0)) _mint(feeRecipient, feeShares);
+        if (feeShares != 0) _mint(feeRecipient, feeShares);
     }
 
     /// @dev Computes and returns the fee shares (`feeShares`) to mint and the new vault's total assets
