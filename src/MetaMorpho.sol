6c83da82e51d6892aec11a0d545c27195dee9d1e
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2023-11-03 14:24:33 +0100

    Apply suggestions from code review
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    Signed-off-by: Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>

--- src/MetaMorpho.sol
+++ src/MetaMorpho.sol
@@ -472,14 +472,14 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
     }
 
     /// @inheritdoc IERC4626
-    /// @dev Warning: may be lower than the actual amount of assets that can be withdrawn by `owner` due to conversion
+    /// @dev Warning: May be lower than the actual amount of assets that can be withdrawn by `owner` due to conversion
     /// roundings between shares and assets.
     function maxWithdraw(address owner) public view override(IERC4626, ERC4626) returns (uint256 assets) {
         (assets,,) = _maxWithdraw(owner);
     }
 
     /// @inheritdoc IERC4626
-    /// @dev Warning: may be lower than the actual amount of shares that can be redeemed by `owner` due to conversion
+    /// @dev Warning: May be lower than the actual amount of shares that can be redeemed by `owner` due to conversion
     /// roundings between shares and assets.
     function maxRedeem(address owner) public view override(IERC4626, ERC4626) returns (uint256) {
         (uint256 assets, uint256 newTotalSupply, uint256 newTotalAssets) = _maxWithdraw(owner);
