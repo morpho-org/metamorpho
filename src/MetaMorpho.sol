179d1ccc80a4c156f6ecaf669581a76c0f717a22
MerlinEgalite <egalite.merlin@gmail.com>
2023-12-27 17:36:39 +0100

    refactor: no revert on revoke

--- src/MetaMorpho.sol
+++ src/MetaMorpho.sol
@@ -412,8 +412,6 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
 
     /// @inheritdoc IMetaMorphoBase
     function revokePendingTimelock() external onlyGuardianRole {
-        if (pendingTimelock.validAt == 0) revert ErrorsLib.NoPendingValue();
-
         delete pendingTimelock;
 
         emit EventsLib.RevokePendingTimelock(_msgSender());
@@ -421,8 +419,6 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
 
     /// @inheritdoc IMetaMorphoBase
     function revokePendingGuardian() external onlyGuardianRole {
-        if (pendingGuardian.validAt == 0) revert ErrorsLib.NoPendingValue();
-
         delete pendingGuardian;
 
         emit EventsLib.RevokePendingGuardian(_msgSender());
@@ -430,8 +426,6 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
 
     /// @inheritdoc IMetaMorphoBase
     function revokePendingCap(Id id) external onlyCuratorOrGuardianRole {
-        if (pendingCap[id].validAt == 0) revert ErrorsLib.NoPendingValue();
-
         delete pendingCap[id];
 
         emit EventsLib.RevokePendingCap(_msgSender(), id);
