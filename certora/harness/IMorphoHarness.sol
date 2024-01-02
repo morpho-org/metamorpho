b787eb129d9d4b393d28f506444a8ede6a031017
Quentin Garchery <garchery.quentin@gmail.com>
2024-01-02 16:49:10 +0100

    feat: add munging

---
+++ certora/harness/IMorphoHarness.sol
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.21;
--- certora/harness/MetaMorphoHarness.sol
+++ certora/harness/MetaMorphoHarness.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.21;
 
-import "../../src/MetaMorpho.sol";
+import "../munged/MetaMorpho.sol";
 
 contract MetaMorphoHarness is MetaMorpho {
     constructor(
