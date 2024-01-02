4fd362d351ef3457e60bb2f9e49125749ceb29b9
Quentin Garchery <garchery.quentin@gmail.com>
2024-01-02 09:41:07 +0100

    refactor: document rounding direction maxDeposit

--- src/MetaMorpho.sol
+++ src/MetaMorpho.sol
@@ -619,8 +619,10 @@ contract MetaMorpho is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorph
 
             uint256 supplyShares = MORPHO.supplyShares(id, address(this));
             (uint256 totalSupplyAssets, uint256 totalSupplyShares,,) = MORPHO.expectedMarketBalances(_marketParams(id));
+            // `supplyAssets` needs to be rounded up for `totalSuppliable` to be rounded down.
+            uint256 supplyAssets = supplyShares.toAssetsUp(totalSupplyAssets, totalSupplyShares);
 
-            totalSuppliable += supplyCap.zeroFloorSub(supplyShares.toAssetsUp(totalSupplyAssets, totalSupplyShares));
+            totalSuppliable += supplyCap.zeroFloorSub(supplyAssets);
         }
     }
 
