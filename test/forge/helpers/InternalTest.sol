97bc9b2782a32ad5392e1dbe2447679091b3282c
Quentin Garchery <garchery.quentin@gmail.com>
2025-07-21 11:51:20 +0200

    test: compatibility via-ir

--- test/forge/helpers/InternalTest.sol
+++ test/forge/helpers/InternalTest.sol
@@ -2,17 +2,41 @@
 pragma solidity ^0.8.0;
 
 import "./BaseTest.sol";
-import {MetaMorpho} from "../../../src/MetaMorpho.sol";
+import {MetaMorpho, SafeERC20} from "../../../src/MetaMorpho.sol";
 
 contract InternalTest is BaseTest, MetaMorpho {
+    using SafeERC20 for IERC20;
+
+    address constant tempOwner = address(100);
+    address constant tempMorpho = address(101);
+
     constructor()
-        MetaMorpho(OWNER, address(morpho), ConstantsLib.MIN_TIMELOCK, address(loanToken), "MetaMorpho Vault", "MM")
-    {}
+        MetaMorpho(
+            tempOwner,
+            tempMorpho,
+            ConstantsLib.MIN_TIMELOCK,
+            address(new ERC20Mock("tT", "tempToken")),
+            "MetaMorpho Vault",
+            "MM"
+        )
+    {
+        DECIMALS_OFFSET = 0;
+        MORPHO = morpho;
+        IERC20(asset()).forceApprove(address(morpho), type(uint256).max);
+    }
+
+    function asset() public view override returns (address) {
+        return address(loanToken);
+    }
 
     function setUp() public virtual override {
         super.setUp();
 
+        vm.prank(tempOwner);
+        this.transferOwnership(OWNER);
+
         vm.startPrank(OWNER);
+        this.acceptOwnership();
         this.setCurator(CURATOR);
         this.setIsAllocator(ALLOCATOR, true);
         vm.stopPrank();
